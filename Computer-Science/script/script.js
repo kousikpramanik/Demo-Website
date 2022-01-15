b4ae6070378b48e1457859636b13c0bcbde4ed92
kousikpramanik <66074903+kousikpramanik@users.noreply.github.com>
2022-01-15 23:11:47 +0530

    Add files via upload

---
+++ Computer-Science/script/script.js
@@ -0,0 +1,6 @@
+function myFunction2(){
+  window.alert("Please copy the mail address & sent a mail in the given mail address.")
+}
+function myFunction() {
+  window.alert("Your message has been sent.");
+}
